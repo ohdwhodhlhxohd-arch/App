e1d1414db27faa32f1e084711576b889f74018eb
ohdwhodhlhxohd-arch <ohdwhodhlhxohd@gmail.com>
2026-02-23 13:38:07 +0200

    Create .config.py

---
+++ .config.py
@@ -0,0 +1,19 @@
+# .config.py
+
+# توكن البوت من BotFather
+API_TOKEN = '8172361853:AAFU7Q7CVjWOQ3rRxKgEgpA6JHzTZLpBfG8'
+
+# الآيدي الخاص بك كمدير (تحصل عليه من @userinfobot)
+ADMIN_ID = 5013565453
+
+# رابط صفحة الإدارة (تحديثه عند كل تشغيل لـ Serveo أو Cloudflared)
+WEBAPP_URL = "https://0f69efac6f27ccf8-41-95-149-249.serveousercontent.com/admin.html"
+
+# مثال لرقم سوداني
+WHATSAPP_NUMBER = "249903405826" 
+
+# إعدادات أخرى اختيارية
+JSON_FILE = "products.json"
+PHOTOS_DIR = "photos"
+
+
